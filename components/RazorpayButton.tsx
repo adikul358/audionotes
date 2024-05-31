f31b62c757754613d20d733b236ce7db034c65f0
Aditya Kulshrestha <adi.kul358@gmail.com>
2024-05-31 08:17:17 +0530

    Razorpay Test Mode

--- components/RazorpayButton.tsx
+++ components/RazorpayButton.tsx
@@ -11,7 +11,7 @@ export default function RazorpayButton() {
       const script = document.createElement("script");
       script.src = "https://checkout.razorpay.com/v1/payment-button.js";
       script.async = true;
-      script.dataset["payment_button_id"] = "pl_OGs39SZoFPovy1";
+      script.dataset["payment_button_id"] = "pl_OGwP53GcfhTRvM";
       rzpPaymentForm.appendChild(script);
 
     }
