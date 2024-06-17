b868f993fd93cdecfe8297f5bc1490bb33eeae79
Aditya Kulshrestha <adi.kul358@gmail.com>
2024-06-17 18:08:24 +0530

    Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
@@ -22,6 +23,13 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
         <link rel="shortcut icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QXV7DLRBQ2"></script>
+        <Script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-QXV7DLRBQ2');
+        </Script>
       </head>
       <body className="bg-background text-foreground">
         {children}
